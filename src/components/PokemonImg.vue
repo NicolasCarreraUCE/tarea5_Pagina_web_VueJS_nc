ab1a07af82960fed044d4cba4a905dac472acee7
Nicolas Carrera <nacarrera@uce.edu.ec>
2023-02-01 04:21:59 -0500

    tarea 10

---
+++ src/components/PokemonImg.vue
@@ -0,0 +1,58 @@
+<template>
+  <!--<img v-if="!showPokemon" v-bind:src="urlImagen" alt="No se puede renderisar" class="ocultar-pkemon">-->
+  <div class="pantalla">
+    <img id="pokemon" v-bind:src="urlImagen" alt="No se puede renderisar" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    idPokemon: {
+      type: Number,
+      required: true,
+      default: 1,
+      validator(value) {
+        return value > 0 && value <= 600;
+      },
+    },
+    showPokemon: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+  },
+  computed: {
+    urlImagen() {
+      const url =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/" +
+        this.idPokemon +
+        ".svg";
+      return url;
+    },
+  },
+  /*data(){
+    return{
+      idPokemon: 1,
+      mostrarPokemon: true
+    }
+  },*/
+};
+</script>
+
+<style>
+.ocultar-pkemon {
+  filter: brightness(0);
+  position: absolute;
+}
+img {
+  width: 100%;
+  /*right: 32%;*/
+  -webkit-user-drag: none;
+  filter: brightness(0);
+}
+.pantalla {
+  border: 30px solid #222f3e;
+  background-color: #fff;
+}
+</style>
---
+++ src/components/PokemonOpts.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="options-conatiner">
+    <button
+      class="options"
+      v-for="poke in pokemons"
+      :key="poke.id"
+      :id="poke.id"
+      @click="opcionSeleccionada(poke.id)"
+    >
+      {{ poke.nombre }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemons: {
+      type: Array,
+      required: true,
+    },
+    idPokemonCorrecto: {
+      type: Number,
+      required: true,
+    },
+    mostrarCorrecto: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      xxx: this.idPokemonCorrecto,
+    };
+  },
+  methods: {
+    opcionSeleccionada(id) {
+      this.$emit("selecionado", id);
+      const btn = document.getElementById(id);
+      if (id == this.idPokemonCorrecto) {
+        btn.style = "background-color: #1dd1a1";
+      } else {
+        btn.style = document.getElementById(id).style =
+          "background-color: #ff6b6b";
+        btn.disabled = true;
+      }
+      this.mostrar;
+    },
+  },
+  updated() {
+    if (this.mostrarCorrecto) {
+      const btns = document.getElementsByClassName("options");
+      const idPokemon = this.idPokemonCorrecto;
+      Array.prototype.forEach.call(btns, function (btn) {
+        if (btn.id == idPokemon) {
+          btn.style = "background-color: #1dd1a1";
+        }
+        btn.disabled = true;
+      });
+    } else {
+      const btns = document.getElementsByClassName("options");
+      Array.prototype.forEach.call(btns, function (btn) {
+        btn.disabled = false;
+      });
+    }
+  },
+};
+</script>
+
+<style>
+.options-conatiner {
+  display: flex;
+  flex-direction: column;
+}
+
+.options {
+  background-color: #48dbfb;
+  color: #fff;
+  font-size: large;
+  margin-top: 15px;
+  padding: 10px;
+  border: none;
+  border-radius: 15px;
+}
+
+.options:hover {
+  cursor: pointer;
+}
+</style>
